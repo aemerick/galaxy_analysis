b57000ea55128e3688f157d2b4799c4aa1cd5dfc
aemerick <aemerick11@gmail.com>
2016-11-08 12:03:44 -0500

    updates and bugfix - working now

--- particle_analysis/sfhFromParticles.py
+++ particle_analysis/sfhFromParticles.py
@@ -11,7 +11,7 @@ def sfhFromParticles(ds, data, selection = None, times = None):
     nstars = np.size( data['particle_mass'] )
 
     if selection is None:
-        selection = [True]*nstars
+        selection = np.array([True]*nstars)
 
     particle_mass = data['birth_mass'][selection].value * yt.units.Msun
     creation_time = data['creation_time'][selection].convert_to_units('Myr')
@@ -31,9 +31,8 @@ def sfhFromParticles(ds, data, selection = None, times = None):
     mass  = np.zeros(np.shape(times))
 
     times = times.convert_to_units('yr')
-
-    for i,t in enumerate(times[1:]):
-        mass[i] = np.sum(particle_mass[creation_time<=t])
+    for i,t in enumerate(times):
+        mass[i] = np.sum( particle_mass[creation_time <= t] )
 
     return times, mass
 
@@ -44,13 +43,16 @@ if __name__=='__main__':
     ds   = yt.load(ds_list[-1])
     data = ds.all_data()
 
-    times = np.arange(0.0*yt.units.Myr, ds.current_time.convert_to_units('Myr'), 25.0*yt.units.Myr)
+    times = np.arange(0.0*yt.units.Myr, ds.current_time.convert_to_units('Myr'), 5.0*yt.units.Myr)
     times = times*yt.units.Myr
 
     times, mass = sfhFromParticles(ds, data, times = times)
+
     fig, ax = plt.subplots(figsize=(8,8))
     ax.plot(times/1.0E6, mass, color = 'black', lw = 3)
     ax.set_xlabel('Time (Myr)')
     ax.set_ylabel(r'Cumulative SFH (M$_{\odot}$)')
-    ax.set_ylim(0, np.max(mass)*1.1)
+    ax.set_ylim(1.0, np.max(mass)*5.0)
+    ax.semilogy()
+    ax.minorticks_on()
     plt.savefig('sfh.png')
--- particle_analysis/sfrFromParticles.py
+++ particle_analysis/sfrFromParticles.py
@@ -12,7 +12,7 @@ def sfrFromParticles(ds, data, selection = None, times = None):
     nstars = np.size( data['particle_mass'] )
 
     if selection is None:
-        selection = [True]*nstars
+        selection = np.array( [True]*nstars )
 
     particle_mass = data['birth_mass'][selection].value * yt.units.Msun
     creation_time = data['creation_time'][selection].convert_to_units('Myr')
@@ -29,14 +29,14 @@ def sfrFromParticles(ds, data, selection = None, times = None):
         times = np.linspace(np.min(creation_time), currentTime, bin_spacing)
         times = times *yt.units.Myr
 
-    sfr   = np.zeros(np.shape(times))
+    sfr   = np.zeros(np.size(times)-1)
 
     times = times.convert_to_units('yr')
 
-    for i,t in enumerate(times[1:]):
-        dt = t - times[i-1]
-        dm = np.sum(particle_mass[creation_time <= t]) -\
-             np.sum(particle_mass[creation_time <= times[i-1]])
+    for i in np.arange(np.size(times)-1):
+        dt = times[i+1] - times[i]
+        dm = np.sum(particle_mass[creation_time <= times[i+1]]) -\
+             np.sum(particle_mass[creation_time <= times[i]])
 
         sfr[i] = dm / dt
 
@@ -44,7 +44,7 @@ def sfrFromParticles(ds, data, selection = None, times = None):
 
 
 if __name__=='__main__':
-
+    log = False
 
     ds_list = np.sort( glob.glob('./DD????/DD????'))
 
@@ -58,12 +58,20 @@ if __name__=='__main__':
 
     times, sfr = sfrFromParticles(ds, data, times = times)
     fig, ax = plt.subplots(figsize=(8,8))
-    ax.plot(times/1.0E6, sfr*1.0E4, color = 'black', lw = 3)
+
+    center = 0.5 * (times[1:]+times[:-1])
+
+    if log:
+        ax.plot(center/1.0E6, sfr*1.0E4, color = 'black', lw = 3)
+        ax.semilogy()
+    else:
+        ax.step(times[:-1]/1.0E6, sfr*1.0E4, color = 'black', lw = 3, where='pre')
+
     ax.set_xlabel('Time (Myr)')
 
     ax.set_ylabel(r'SFR (10$^{-4}$ M$_{\odot}$/yr)')
-    ax.set_ylim(0, np.max(sfr)*1.1 * 1.0E4)
-
+    ax.set_ylim(0.0, np.max(sfr)*1.1 * 1.0E4)
+    ax.minorticks_on()
     plt.savefig('sfr.png')
 
 
