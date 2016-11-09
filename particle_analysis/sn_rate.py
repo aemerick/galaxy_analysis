1203a1a893e110ee4ea477ce001900b1ca79920a
aemerick <aemerick11@gmail.com>
2016-11-09 16:40:23 -0500

    cleaning

--- particle_analysis/sn_rate.py
+++ particle_analysis/sn_rate.py
@@ -3,6 +3,70 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
+_core_collapse_labels = ["SNII", "II", "2", "SN_II", "TypeII", "Type 2",
+                         "Type II", "type II", "typeII", 'core collapse']
+
+_snia_labels = ["SN1a", "SNIa", "Type1a", "TypeIa", "Type Ia", "Type 1a",
+                 "type 1a", "type Ia", "type ia", "type1a", "typeIa"]
+
+_agb_labels = ['AGB', 'agb']
+
+
+def future_snr(ds, data, times = None, sn_type = 'II'):
+    """
+    Looks forward from current time to compute future (projected) SN
+    rate
+    """
+
+    current_time  = ds.current_time.convert_to_units('Myr').value
+
+    if times is None:
+        bin_spacing = 10.0* yt.units.Myr
+        times = np.linspace(current_time, current_time + 2000.0, bin_spacing)*yt.units.Myr
+    elif np.size(times) == 1:
+        bin_spacing = times
+        if not hasattr(bin_spacing, 'value'):
+            bin_spacing = bin_spacing * yt.units.Myr
+
+        times = np.linspace(current_time, current_time + 2000.0, bin_spacing)
+        times = times * yt.units.Myr
+
+    birth_mass    = data['birth_mass'].value
+    mass          = data['particle_mass'].convert_to_units('Msun').value
+    creation_time = data['creation_time'].convert_to_units('Myr').value
+    lifetimes     = data['dynamical_time'].convert_to_units('Myr').value
+    pt            = data['particle_type']
+
+
+    if  any( [sn_type in x for x in _core_collapse_labels]):
+
+        collapse_threshold = ds.parameters['IndividualStarDirectCollapseThreshold']
+        agb_threshold      = ds.parameters['IndividualStarSNIIMassCutoff']
+
+        pcut = (pt == 11) * (birth_mass <= collapse_threshold) *\
+                            (birth_mass  > agb_threshold)
+
+    elif any( [sn_type in x for x in _snia_labels]):
+
+        pcut = (pt == 12) * (mass > 0.0)
+
+    explosion_times = creation_time[pcut] + lifetimes[pcut]
+    explosion_times = explosion_times * yt.units.Myr
+
+    times = times.convert_to_units('yr')
+    snr   = np.zeros(np.size(times.value) - 1)
+
+    # compute SNR
+    for i in np.arange(np.size(times) - 1):
+        dt = times[i+1] - times[i]
+        dN = np.size( explosion_times[explosion_times <= times[i+1]]) -\
+             np.size( explosion_times[explosion_times <= times[i]])
+
+        snr[i] = dN / dt
+
+    return times, snr
+
+
 def snr(ds, data, times = None, sn_type = 'II'):
     """
     Computes the supernova rate of the desired time for a given dataset
@@ -19,22 +83,17 @@ def snr(ds, data, times = None, sn_type = 'II'):
     parameter file.
     """
 
-    _core_collapse_labels = ["SNII", "II", "2", "SN_II", "TypeII", "Type 2",
-                             "Type II", "type II", "typeII", 'core collapse']
-    _snia_labels = ["SN1a", "SNIa", "Type1a", "TypeIa", "Type Ia", "Type 1a",
-                     "type 1a", "type Ia", "type ia", "type1a", "typeIa"]
-
-    _agb_labels = ['AGB', 'agb']
+    current_time  = ds.current_time.convert_to_units('Myr').value
 
     if times is None:
         bin_spacing = 10.0 * yt.units.Myr
-        times = np.linspace(np.min(creation_time), currentTime, bin_spacing)*yt.units.Myr
+        times = np.linspace(np.min(creation_time), current_time, bin_spacing)*yt.units.Myr
     elif np.size(times) == 1:
         bin_spacing = times
         if not hasattr(bin_spacing, 'value'):
             bin_spacing = bin_spacing * yt.units.Myr
 
-        times = np.linspace(np.min(creation_time), currentTime, bin_spacing)
+        times = np.linspace(np.min(creation_time), current_time, bin_spacing)
         times = times *yt.units.Myr
 
 
@@ -45,12 +104,11 @@ def snr(ds, data, times = None, sn_type = 'II'):
     metallicity   = data['metallicity_fraction'].value
     lifetimes     = data['dynamical_time'].convert_to_units('Myr').value
     pt            = data['particle_type'].value
-    current_time  = ds.current_time.convert_to_units('Myr').value
 
     # check to see if there are any SN candidates in the first place
-    if not any([sn_particle_type == x for x in np.unique(pt)]):
-        print "no supernova of type " + sn_type + " found"
-        return times, np.zeros(np.size(times.value) - 1)
+#    if not any([ == x for x in np.unique(pt)]):
+#        print "no supernova of type " + sn_type + " found"
+#        return times, np.zeros(np.size(times.value) - 1)
 
     # looking for core collapse supernova rate
     if  any( [sn_type in x for x in _core_collapse_labels]):
@@ -145,6 +203,12 @@ if __name__ == '__main__':
     snialabel = 'Type Ia'
     sniilabel = 'Core Collapse'
 
+    ftimes = np.arange(ds.current_time.convert_to_units('Myr').value,
+                             ds.current_time.convert_to_units('Myr').value + 800.0 + 10, 10)
+    ftimes = ftimes * yt.units.Myr
+    ftimes, fsnrII = future_snr(ds, data, times = ftimes, sn_type = 'TypeII')
+    ftimes, fsnrIa = future_snr(ds, data, times = ftimes, sn_type = 'TypeIa')
+
     if log:
         ax.plot(center/1.0E6, snrII*1.0E6, color = 'black', lw = 3, ls = '-', label = sniilabel)
         ax.plot(center/1.0E6, snrIa*1.0E6, color = 'black', lw = 3, ls = '--', label = snialabel)
@@ -152,6 +216,8 @@ if __name__ == '__main__':
     else:
         ax.step(times[:-1]/1.0E6, snrII*1.0E6, color ='black', lw = 3, ls = '-', label = sniilabel)
         ax.step(times[:-1]/1.0E6, snrIa*1.0E6, color ='orange', lw = 3, ls = '-', label = snialabel)
+        ax.step(ftimes[:-1]/1.0E6, fsnrII*1.0E6, color = 'black', lw = 3, ls = ':')
+        ax.step(ftimes[:-1]/1.0E6, fsnrIa*1.0E6, color = 'orange', lw = 3, ls = ':')
 
     ax.set_xlabel('Time (Myr)')
 
