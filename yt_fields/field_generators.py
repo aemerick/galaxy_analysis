098ef7267b99d4291eb77a12db9c126533bd8490
aemerick <aemerick11@gmail.com>
2016-11-10 13:47:00 -0500

    fully functioning ndens, m frac, and abundance ratios

--- yt_fields/field_generators.py
+++ yt_fields/field_generators.py
@@ -1,66 +1,160 @@
+
+__author__ = "Andrew Emerick"
+__email__  = "aemerick11@gmail.com"
+
 import yt
+from yt.units import dimensions
 
 from collections import Iterable
 
 from galaxy_analysis.static_data import AMU,\
                  MOLECULAR_WEIGHT
 
+from galaxy_analysis.utilities import convert_abundances
+from galaxy_analysis.utilities import utilities as util
+
+
+#
+# Construct arbitrary mass fraction derived fields in yt
+# using a loop to generate functions
+#
 def _mass_fraction_function_generator(asym):
 
     if not isinstance(asym, Iterable):
         asym = [asym]
 
-    for a in asym:
 
+    def return_function(a):
         def _mass_fraction(field,data):
             ele_dens = data[('enzo', a + '_Density')].value
             ele_dens = ele_dens * data.ds.mass_unit / data.ds.length_unit**3
             ele_dens = ele_dens.convert_to_cgs()
 
             dens = data[('enzo','Density')].convert_to_cgs()
-
             mass_fraction = ele_dens / dens
-
             return mass_fraction
 
-        yt.add_field(('gas', a + '_Fraction'), function = _mass_fraction, units='dimensionless')
+        return _mass_fraction
 
-    return
+    nfields = 0
+    for a in asym:
 
+        yt.add_field(('gas', a + '_Fraction'), function = return_function(a), units='auto',
+                                               dimensions=dimensions.dimensionless)
+
+        nfields = nfields + 1
+
+    return nfields
+
+#
+# Construct arbitrary number density derived fields in yt
+# using a loop to generate functions
+#
 def _number_density_function_generator(asym):
 
     if not isinstance(asym, Iterable):
         asym = [asym]
 
-    for a in asym:
 
+    def return_function(a):
         def _number_density(field,data):
             ele_dens = data[('enzo', a + '_Density')].value
             ele_dens = ele_dens * data.ds.mass_unit / data.ds.length_unit**3
             ele_dens = ele_dens.convert_to_cgs()
-
             n = ele_dens / (MOLECULAR_WEIGHT[a] * AMU * yt.units.g)
 
             return n.convert_to_cgs()
 
+        return _number_density
+
+    nfields = 0
+    for a in asym:
+
         yt.add_field(('gas', a + '_Number_Density'),
-                     function = _number_density, units='cm**(-3)')
+                     function = return_function(a), units='cm**(-3)')
+        nfields = nfields + 1
 
-
+    return nfields
+#
+# Construct arbitrary abundance ratio fields in yt
+# using a function generator
+#
 def _abundance_ratio_function_generator(ratios):
 
     if not isinstance(ratios, Iterable):
         ratios = [ratios]
 
+
+    def return_function(ele1, ele2, field1, field2):
+        def _abundance_ratio(field, data):
+
+            mass1 = data[field1]
+            if ele1 != 'H' and ele1 != 'He':
+                mass1 = mass1.value * data.ds.mass_unit / data.ds.length_unit**3
+            mass1 = (mass1 * data['cell_volume']).convert_to_units('g')
+
+            mass2 = data[field2]
+            if ele2 != 'H' and  ele2 != 'He':
+                mass2 = mass2.value * data.ds.mass_unit / data.ds.length_unit**3
+            mass2 = (mass2 * data['cell_volume']).convert_to_units('g')
+
+            ratio = convert_abundances.abundance_ratio( (ele1, mass1.value), (ele2, mass2.value), 'mass')
+
+            return ratio * yt.units.g / yt.units.g
+
+        return _abundance_ratio
+
+    nfields = 0
     for r in ratios:
 
         ele1, ele2 = r.rsplit('/')
 
-        def _abundance_ratio(field, data):
+        if ele1 != 'H' and ele2 != 'He':
+            field1 = ('enzo', ele1 + '_Density')
+        else:
+            field1 = ('gas', ele1 + '_density')
 
-            mass1 = data[('gas', ele1 + '_Density')].value * data.ds.mass_unit / data.ds.length_unit**3
-            mass1 = (mass1 * data['cell_volume']).convert_to_cgs()
+        if ele2 != 'H' and ele2 != 'He':
+            field2 = ('enzo', ele2 + '_Density')
+        else:
+            field2 = ('gas', ele2 + '_density')
 
-            mass2 = data[('gas', ele2 + '_Density')].value * data.ds.mass_unit / data.ds.length_unit**3
-            mass2 = (mass2 * data['cell_volume']).convert_to_cgs()
+        fieldname = ele1 + '_over_' + ele2
 
+        yt.add_field(('gas', fieldname), function = return_function(ele1,ele2,field1,field2),
+                              units = 'dimensionless')
+        nfields = nfields + 1
+
+    return nfields
+
+def generate_derived_fields(ds):
+    """
+    Given a data set (to extract the on-disk field names), generate
+    derived fields that will persist for the python session (i.e. not
+    tied only to the passed data set).
+
+    Right now, takes in all metal species tracer fields and constructs
+    fields for their mass fraction, number density, and all possible
+    interesting abundance ratios.
+
+    NOTE: The derived fields will only exist for data sets loaded after
+    this function call. If analysis is intended for passed data set,
+    it will need to be reloaded for fields to exist.
+    """
+
+    fields = ds.field_list
+
+    # lets figure out the metal tracers present
+    metals = util.species_from_fields(fields)
+    ratios = util.ratios_list(metals)
+
+    print "tracer species present: ", metals
+    nfields = _mass_fraction_function_generator(metals)
+    print nfields, "mass fraction fields defined"
+    nfields = _number_density_function_generator(metals)
+    print nfields, "number density fields defined"
+    nfields = _abundance_ratio_function_generator(ratios)
+    print nfields, "abundance ratio fields defined"
+
+
+    return
