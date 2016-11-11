eba0eb7a4f5412e93c689855d64708bc15d327a3
aemerick <aemerick11@gmail.com>
2016-11-11 10:44:51 -0500

    api.py

---
+++ particle_analysis/api.py
@@ -0,0 +1,13 @@
+"""
+API for particle analysis
+
+"""
+
+from .sfrFromParticles import \
+     sfrFromParticles
+
+from .sfhFromParticles import \
+     sfhFromParticles
+
+from .abundances import \
+    generate_abundances
