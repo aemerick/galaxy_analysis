22a6ac80a7f29eda85b9c9de3998b041a27bf226
aemerick <aemerick11@gmail.com>
2016-11-16 15:45:27 -0500

    surface density profile. new class for individual data sets

--- galaxy_analysis.py
+++ galaxy_analysis.py
@@ -1,8 +1,12 @@
 from __future__ import division
 
-
+import yt
 import numpy as np
 import glob
+import os
+import h5py
+
+from collections import Iterable
 
 _hdf5_compression = 'lzf'
 
@@ -25,7 +29,7 @@ def _create_data_structure(f, ds_list):
     f.create_group("TimeEvolution")
     grp = f.create_group("Spatial")
 
-    f.create_dataset('Time',
+    f.create_dataset('Time')
 
     for dsname in ds_list:
         dsname = dsname.rsplit('/')[0]
@@ -75,7 +79,7 @@ def _update_data_structure(f, ds_list):
     return True
 
 
-class Galaxy(object):
+class GalaxyAnalyzer(object):
     """
     Analyzes time evolution properties of a galaxy
     """
@@ -87,6 +91,9 @@ class Galaxy(object):
         self.ds0        = yt.load(ds_list[0])
         self.dsf        = yt.load(ds_list[-1])
         self.parameters = ds0.parameters
+
+        _set_data_region_properties()
+
 
         if h5file is None:
             hf5file = dir + 'galaxy_data.h5'
@@ -109,12 +116,13 @@ class Galaxy(object):
 
         self.data = f
 
-       # maybe store time values from all data sets here            
+        # maybe store time values from all data sets here            
 
-       return
+        return
 
 
     def create_radial_profile(self, fields = None, dsi = None):
+        raise NotImplementedError
 
         if dsi is None:
             data_files = ds_list
@@ -128,12 +136,16 @@ class Galaxy(object):
             ds   = yt.load(dsname)
             data = ds.all_data()
 
-            profile_1d = yt.create_profile(data, ['density', 'temperature',
-                                                  '
+        return
+            #profile_1d = yt.create_profile(data, ['density', 'temperature',
+            #                                      '
 
     def particle_radial_profile(self, fields = None, dsi= None):
+        raise NotImplementedError
+        return
 
-    def sfr_from_last():
+    def sfr_from_last(self):
+        """ Calculate SFR from final data dump """
 
         if 'SFR' in self.data['TimeEvolution'].keys():
             if self.ds1.current_time > self.data['TimeEvolution']['SFR'][lastval]:
@@ -143,7 +155,6 @@ class Galaxy(object):
             else:
                 # up to date
                 return
-        
         # now compute the SFR
         times, sfr = particle_analysis.sfrFromParticles(self.dsf, self.dsf.all_data())
 
@@ -153,7 +164,10 @@ class Galaxy(object):
 
         return
 
-    def snr_from_last():
+    def snr_from_last(self):
+        """ Calculate the SNR from final data dump """
+
+        raise NotImplementedError
 
         for sn_type in ['SNII, SNIa']:
             rate = sn_type + '_rate'
@@ -166,5 +180,215 @@ class Galaxy(object):
             else:
                 return
 
-            times, snr = particle_analysis.snrFromParticles(
-    
+#            times, snr = particle_analysis.snrFromParticles(
+        return
+
+class Galaxy(object):
+
+    def __init__(self, dsname, dir = './'):
+        """
+        Object for individual galaxy object
+        """
+
+        self.dir    = dir
+        self.dsname = dsname
+
+        self.ds     = yt.load(self.dir + '/' + self.dsname + '/' + self.dsname)
+
+        hdf5_file   = self.dir + '/' + self.dsname + '_galaxy_data.h5'
+
+        self._set_data_region_properties()
+
+        self.construct_regions()
+
+
+        if not os.path.isfile( hdf5_file ):
+            f = h5py.File(hdf5_file, 'w')
+
+            self._create_data_structure()
+
+        else:
+
+            f = h5py.File(hdf5_file, 'a')
+            self._update_data_structure()
+
+        self.data = f
+
+        return
+
+    def _create_data_structure(self):
+        print 'does nothing for now'
+        return
+
+    def _update_data_structure(self):
+        print "does nothing for now"
+        return
+
+    def calculate_projected_disk_field(self, field, **kwargs):
+        """
+        Calculate the projected surface density of a given field.
+        Examples:
+           For total gas density:
+           > galaxy.calculate_surface_density(('gas','Density'))
+           For H2 density:
+           > galaxy.calculate_surface_density(('gas','H2I_Density'))
+        """
+
+        proj = ds.proj( field, 'z', data_source = self.disk, **kwargs)
+
+        # return raw set to handle elsewhere
+        return proj
+
+    def calculate_mass_fractions(self, fields = None, *args, **kwargs):
+       """
+       Compute mass fraction of given species contained within spherical
+       bins
+       """
+
+       return
+
+
+    def calculate_surface_density_profile(self, fields, *args, **kwargs):
+        """
+        Computes a 1D surface density profile in a cylindrical region of the galaxy
+        using already set disk selection region.
+        """
+
+        if not isinstance(fields, Iterable):
+            fields = [fields]
+
+        # project the fields
+        proj = self.ds.proj(fields, 'z', data_source = self.disk, **kwargs)
+
+        r    = np.sqrt(( (proj['px']-self.disk.center[0])**2 + (self.disk.center[1] -proj['py'])**2)).convert_to_units('pc')
+        A    = (proj['pdx']*proj['pdy']).convert_to_units('pc**2')
+
+
+        # set up bins
+        r_max = self.disk_region['radius']
+        dr    = self.disk_region['dr']
+        r_max = r_max.convert_to_units(dr.units)
+
+        rbins = self.rbins_disk()
+
+        profiles = {}
+
+        # construct the profiles
+        for field in fields:
+            profiles[field] = np.zeros(np.size(rbins) - 1)
+
+        for i in np.arange(np.size(rbins)-1):
+            for field in fields:
+
+                radial_filter = (r >= rbins[i]) * (r < rbins[i+1])
+
+                annulus_area  = 2.0 * np.pi * (rbins[i+1]**2 - rbins[i]**2)
+
+                if np.size( proj['dx'][radial_filter] ) > 0:
+
+                    projection = np.array(proj[field][radial_filter].convert_to_units('Msun/pc**2'))
+
+                    profiles[field][i] =\
+                       (np.sum(projection * np.array(A[radial_filter]))\
+                                 / annulus_area.value) * yt.units.Msun / yt.units.pc**2
+
+        centers = 0.5 * (rbins[:-1] + rbins[1:])
+
+        return rbins, centers, profiles
+
+    def construct_regions(self, disk_kwargs = None, sphere_kwargs = None,
+                                halo_sphere_kwargs = None):
+
+        if disk_kwargs is None:
+            disk_kwargs = {}
+
+            for n in ['normal','radius','height','center']:
+                disk_kwargs[n] = self.disk_region[n]
+
+        if sphere_kwargs is None:
+            sphere_kwargs = {}
+
+            for n in ['radius','center']:
+                sphere_kwargs[n] = self.spherical_region[n]
+
+        if halo_sphere_kwargs is None:
+            halo_sphere_kwargs = {}
+
+            for n in ['radius','center']:
+                sphere_kwargs[n] = self.halo_spherical_region[n]
+
+
+        self.disk   = self.ds.disk(**disk_kwargs)
+
+        self.sphere = self.ds.sphere(**sphere_kwargs)
+
+        self.halo_sphere = self.ds.sphere(**halo_sphere_kwargs)
+
+        return
+
+    def _set_data_region_properties(self):
+        """
+        Sets parameters used to define disk and spherecal data regions
+        Separate function currently useless, but allows this to be
+        expanded so one can guess sizes from parameter file settings,
+        rather than hard coding as is done below.
+        """
+
+        # dr and dz set spacing for cylindrical or spherical regions
+        # to use to construct shells
+
+        self.disk_region = {'normal' : np.array([0.0, 0.0, 1.0]),
+                            'radius' : 2.0 * yt.units.kpc,
+                            'height' : 500.0 * yt.units.pc,
+                            'center' : self.ds.domain_center,
+                            'dr'     : 25.0 * yt.units.pc,
+                            'dz'     : 100.0 * yt.units.pc }
+
+        self.spherical_region = {'center' : self.ds.domain_center,
+                                 'radius' : 2.0 * yt.units.kpc,
+                                 'dr'     : 25.0 * yt.units.pc   }
+
+        self.halo_spherical_region = {'center' : self.ds.domain_center,
+                                      'radius' : 5.0 * yt.units.kpc,
+                                      'dr'     : 50.0 * yt.units.pc}
+
+        return
+
+
+    @property
+    def rbins_sphere(self):
+
+        rmin = 0.0
+        rmax = self.sphere.radius
+        dr   = self.spherical_region['dr']
+        rmax = rmax.convert_to_units(dr.units)
+
+        return np.arange(rmin, rmax + dr, dr) * dr.unit_quantity
+
+    @property
+    def rbins_halo_sphere(self):
+        rmin = 0.0
+        rmax = self.halo_sphere.radius
+        dr   = self.halo_spherical_region['dr']
+        rmax = rmax.convert_to_units(dr.units)
+
+        return np.arange(rmin, rmax + dr, dr) * dr.unit_quantity
+
+    @property
+    def zbins_disk(self):
+        zmin = 0.0
+        zmax = self.disk.height * 0.5
+        dz   = self.disk_region['dz']
+        zmax = zmax.convert_to_units(dz.units)
+
+        return np.arange(zmin, zmax + dz, dz) * dz.unit_quantity
+
+
+    @property
+    def rbins_disk(self):
+        rmin = 0.0
+        rmax = self.disk.radius
+        dr   = self.disk_region['dr']
+        rmax = rmax.convert_to_units(dr.units)
+
+        return np.arange(rmin, rmax + dr, dr) * dr.unit_quantity
