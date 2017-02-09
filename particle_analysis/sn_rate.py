30970e277ea62f6596ed74f9b59ca35407ae58a5
aemerick <aemerick11@gmail.com>
2017-02-09 09:34:50 -0500

    plot adjustments

--- particle_analysis/sn_rate.py
+++ particle_analysis/sn_rate.py
@@ -200,7 +200,7 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(figsize=(8,8))
 
-    snialabel = 'Type Ia'
+    snialabel = 'Type Ia x 10'
     sniilabel = 'Core Collapse'
 
     ftimes = np.arange(ds.current_time.convert_to_units('Myr').value,
@@ -211,19 +211,21 @@ if __name__ == '__main__':
 
     if log:
         ax.plot(center/1.0E6, snrII*1.0E6, color = 'black', lw = 3, ls = '-', label = sniilabel)
-        ax.plot(center/1.0E6, snrIa*1.0E6, color = 'black', lw = 3, ls = '--', label = snialabel)
+        ax.plot(center/1.0E6, snrIa*1.0E6*10, color = 'black', lw = 3, ls = '--', label = snialabel)
         x.semilogy()
     else:
         ax.step(times[:-1]/1.0E6, snrII*1.0E6, color ='black', lw = 3, ls = '-', label = sniilabel)
-        ax.step(times[:-1]/1.0E6, snrIa*1.0E6, color ='orange', lw = 3, ls = '-', label = snialabel)
+        ax.step(times[:-1]/1.0E6, snrIa*1.0E6 * 10, color ='orange', lw = 3, ls = '-', label = snialabel)
         ax.step(ftimes[:-1]/1.0E6, fsnrII*1.0E6, color = 'black', lw = 3, ls = ':')
-        ax.step(ftimes[:-1]/1.0E6, fsnrIa*1.0E6, color = 'orange', lw = 3, ls = ':')
+        ax.step(ftimes[:-1]/1.0E6, fsnrIa*1.0E6 * 10, color = 'orange', lw = 3, ls = ':')
 
     ax.set_xlabel('Time (Myr)')
 
-    ax.set_ylabel(r'SNR (10$^{-6}$ yr$^{-1}$)')
+    ax.set_ylabel(r'SNR (Myr$^{-1}$)')
     ax.set_ylim( np.min( [np.min(snrIa), np.min(snrII)])*1.0E6,
-                 np.max( [np.max(snrIa), np.max(snrII)])*1.5*1.0E6)
+                 np.max( [np.max(snrIa), np.max(snrII)])*1.25*1.0E6)
+    ax.plot( [ds.current_time.convert_to_units('Myr').value]*2, ax.get_ylim(), ls = '--', lw = 3, color = 'black')
+
     ax.legend(loc ='best')
     plt.tight_layout()
     ax.minorticks_on()
