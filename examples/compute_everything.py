d3704b8034d5616e9169455a405d5a17470dace4
Andrew Emerick <emerick@login1.scdanet.org>
2017-02-21 17:51:15 -0500

    first commit

---
+++ examples/compute_everything.py
@@ -0,0 +1,22 @@
+import galaxy_analysis as ga
+import glob
+import os
+
+if True:
+
+    imin = 127
+    imax = 128
+    di   = 1
+
+    for i in np.arange(imin,imax+di,di):
+
+        if not os.path.isfile('DD%0004i/DD%0004i'%(i,i)):
+            print "File path not found : DD%0004i/DD%0004i"%(i,i)
+            continue
+
+        galaxy = ga.Galaxy('DD%0004i'%(i))
+        galaxy.compute_everything()
+        galaxy.save()
+        del(galaxy)
+
+
