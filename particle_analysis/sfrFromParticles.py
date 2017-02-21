d7e251eb99bf41cd438887bcc1a707d15915c631
aemerick <aemerick11@gmail.com>
2017-02-21 14:55:17 -0500

    bin size change

--- particle_analysis/sfrFromParticles.py
+++ particle_analysis/sfrFromParticles.py
@@ -51,7 +51,7 @@ if __name__=='__main__':
     ds   = yt.load(ds_list[-1])
     data = ds.all_data()
 
-    dt = 25.0*yt.units.Myr
+    dt = 2.5*yt.units.Myr
 
     times = np.arange(0.0*yt.units.Myr, ds.current_time.convert_to_units('Myr')+dt, dt)
     times = times*yt.units.Myr
