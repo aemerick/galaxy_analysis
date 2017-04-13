5b621a66229b01f8cc69ddc2cb580fc4da209725
aemerick <aemerick11@gmail.com>
2017-04-13 13:02:57 -0400

    bugfixes and additional documentation in parameter file. Only tested for when dump and analysis files exist. Works in this case

--- utilities/prepare_onezone.py
+++ utilities/prepare_onezone.py
@@ -41,7 +41,7 @@ def check_analysis_output(dsname, make_analysis = True):
     it.
     """
 
-    if os.path.isfile(dsname + '_galaxy_analysis.h5'):
+    if os.path.isfile(dsname + '_galaxy_data.h5'):
         return True
 
     #
@@ -64,9 +64,9 @@ def get_parameters_from_analysis(galname, onez = None, gal_files = None):
     if onez is None:
         onez = {}
 
-    if os.path.isfile('DD0001_galaxy_analysis.h5'):
-        data = dd.io.load('DD0001_galaxy_analysis.h5')
-        m_o  = dd['particle_meta_data']['total_birth_mass']
+    if os.path.isfile('DD0001_galaxy_data.h5'):
+        data = dd.io.load('DD0001_galaxy_data.h5')
+        m_o  = data['particle_meta_data']['total_birth_mass']
 
         onez['config.zone.initial_stellar_mass'] = m_o
 
@@ -80,19 +80,21 @@ def get_parameters_from_analysis(galname, onez = None, gal_files = None):
 
     data = dd.io.load(galname)
     t_first_star = data['particle_meta_data']['t_first_star']
-    
+
     all_t = np.zeros(len(gal_files))
     for i,f in enumerate(gal_files):
         all_t[i] = dd.io.load(f, '/meta_data/Time')
 
     imin = np.argmin( np.abs(all_t - t_first_star))
-    
+
     onez['config.zone.initial_gas_mass'] = dd.io.load(gal_files[imin], '/meta_data/M_gas')
     onez['config.zone.initial_metallicity'] = dd.io.load(gal_files[imin], '/meta_data/Z_avg')
-    
+
+    onez['config.zone.t_final'] = np.max(all_t)
+
     return onez, t_first_star
 
-def enzo_to_onezone(paramaters, onez = None):
+def enzo_to_onezone(parameters, onez = None):
 
     if onez is None:
         onez = {}
@@ -100,12 +102,12 @@ def enzo_to_onezone(paramaters, onez = None):
     onez['config.units.time'] = parameters['TimeUnits']
 
 
-    onez['config.zone.stochastic_sample_mass']  = parameters['IndividualStarSFGassMassThreshold']/\
+    onez['config.zone.stochastic_sample_mass']  = parameters['IndividualStarSFGasMassThreshold']/\
                                                   parameters['IndividualStarMassFraction']
 
     onez['config.zone.M_min'] = parameters['IndividualStarIMFUpperMassCutoff']
     onez['config.zone.M_max'] = parameters['IndividualStarIMFLowerMassCutoff']
-    
+
     onez['config.stars.SNII_mass_threshold']        = parameters['IndividualStarSNIIMassCutoff']
     onez['config.stars.SNIa_candidate_mass_bounds'] = [parameters['IndividualStarSNIaMinimumMass'],
                                                        parameters['IndividualStarWDMaximumMass']]
@@ -136,7 +138,7 @@ def set_default_parameters(onez = None):
         onez = {}
 
     onez['config.zone.star_formation_method'] = 4
-    onez['config.zone.SFR_filename']          = 'SFR.in'
+#    onez['config.zone.SFR_filename']          = "SFR.in"
     onez['config.zone.constant_SFR']          = 0.0
 
     onez['config.zone.cosmological_evolution'] = False
@@ -162,13 +164,17 @@ def set_default_parameters(onez = None):
 
     onez['config.io.dt_summary']    = 10.0
     onez['config.io.cycle_summary'] = 0
-    onez['config.io.summary_output_filename'] = 'summary_output.txt'
+#    onez['config.io.summary_output_filename'] = 'summary_output.txt'
     onez['config.io.dt_dump']       = 100.0
+    onez['config.io.radiation_binned_output'] = 1
 
+    onez['config.zone.species_to_track'] = ['m_tot', 'm_metal', 'H', 'He', 'C', 'N', 'O',
+                                            'Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Mn', 'Fe',
+                                            'Co', 'Ni', 'Cu', 'Sr', 'Y', 'Ba', 'Eu']
 
     #
     # set these to none to ensure they are set somehow
-    # 
+    #
     for k in _none_parameters:
         onez[k] = None
 
@@ -181,27 +187,58 @@ def generate_sfr(ds, t_o = 0.0):
 
     selection = data['creation_time'].convert_to_units('Myr').value > t_o
 
-    t, sfr = sfrFromParticles(ds, data = ds.all_data(), selection = selection, times = 10.0)
+    t, SFR = ga.particle_analysis.sfrFromParticles(ds, data = ds.all_data(), selection = selection, times = 10.0, t_o = 0.0)
 
 
-    f = open('SFR.in', 'w')
+    f = open('./onez_model/SFR.in', 'w')
 
     f.write("#t SFR\n")
     f.write("%5.5E %5.5E\n"%(t[0], SFR[0]))
 
     for i in np.arange(np.size(SFR)):
-        f.write("%5.5E %5.5E\n"( 0.5 * (t[i] + t[i+1]), SFR[i]))
+        f.write("%5.5E %5.5E\n"%( 0.5 * (t[i] + t[i+1]), SFR[i]))
 
     f.close()
-    
+
     return
 
 def save_script(onez, outname = 'onez_param_file.py'):
 
     f = open(outname,'w')
 
-    for k in onez.keys():
-        f.write(k + srt(onez[k]) + "\n")
+    f.write("# This python script is the parameter file and\n" +\
+            "# initialization routine for a onez chemical evolution\n" +\
+            "# model matching the initial star formation conditions\n" +\
+            "# from an isolated dwarf galaxy feedback + chemical\n" +\
+            "# evolution simulation run with Enzo. This can be used to\n" +\
+            "# generate multiple (simplistic) realizations of the chemical\n" +\
+            "# evolution based on the galaxy IC's and SFR from the\n" +\
+            "# more expensive radiative hydrodynamics simulations.\n" +\
+            "# Currently, the onez model is a closed-box model, but can\n" +\
+            "# be extended to account for the mass loading factors produced\n" +\
+            "# in the simulation (future work).\n\n" +\
+            "# The onezone model can be run simply as:\n" +\
+            "#      python onez_parameter_file.py\n")
+
+    f.write("from onezone import zone\n")
+    f.write("from onezone import imf\n")
+    f.write("from onezone import config\n\n\n\n")
+
+    f.write("# -------------------- Set parameters ---------------------\n")
+    # organize the output parameters slightly
+    for x in ['config.units','config.zone','config.stars','config.io']:
+
+        for k in onez.keys():
+            if x in k:
+                f.write(k + "          = " + str(onez[k]) + "\n")
+
+        f.write("\n\n")
+
+    f.write("# -------------------- Set up and run the simulation -------------------\n")
+    f.write("sim = zone.Zone()\n"+\
+            "sim.set_initial_abundances(config.zone.species_to_track)\n"+\
+            "# run:\n"+\
+            "sim.evolve()\n")
 
     f.close()
 
@@ -212,19 +249,19 @@ if __name__=="__main__":
 
     # glob for both data files AND galaxy hdf5 files
 
-    make_analysis = True # change to command line parameter
+    make_analysis = False # change to command line parameter
 
     ds_files = glob.glob('DD????')
     ds_files = np.sort(ds_files)
 
-    gal_files = glob.glob('DD????_galaxy_analysis.h5')
+    gal_files = glob.glob('DD????_galaxy_data.h5')
     gal_files = np.sort(gal_files)
 
     if len(ds_files) > 1:
-        ds = ds_files[-1]
+        dsname = ds_files[-1]
 
         if check_analysis_output(ds_files[-1], make_analysis = make_analysis):
-            analysis_to_use = ds_files[-1] + '_galaxy_analysis.h5'
+            analysis_to_use = ds_files[-1] + '_galaxy_data.h5'
         else:
             if len(gal_files) < 1:
                 print "Need to allow for generating the analysis file, or have files already generated"
@@ -244,14 +281,12 @@ if __name__=="__main__":
 
         analysis_to_use = gal_files[-1]
 
+    ds = yt.load(dsname + '/' + dsname)
 
     onez = set_default_parameters()
-    onez = enzo_to_onezoe(ds.parameters, onez = onez)
+    onez = enzo_to_onezone(ds.parameters, onez = onez)
     onez, t_o = get_parameters_from_analysis(analysis_to_use, onez = onez, gal_files = gal_files)
 
     generate_sfr(ds, t_o = t_o)
 
-    save_script(onez, outname = "onez_parameter_file.py")
-        
-
-    
+    save_script(onez, outname = "./onez_model/onez_parameter_file.py")
