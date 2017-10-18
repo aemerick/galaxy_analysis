1f0114939314098cc7ce8c81564a19096d8bd5db
Andrew Emerick <aemerick11@gmail.com>
2017-10-18 00:35:39 -0400

    final updates to passable figure

--- method_paper_plots/panel_plot.py
+++ method_paper_plots/panel_plot.py
@@ -1,5 +1,8 @@
 from galaxy_analysis.plot.plot_styles import *
 rc('font', size=12) # make smaller for these plots
+import matplotlib
+#matplotlib.use('Agg')
+
 from galaxy_analysis.analysis import Galaxy
 
 import yt
@@ -9,35 +12,34 @@ from mpl_toolkits.axes_grid1 import AxesGrid
 yt.enable_parallelism()
 
 fns = ['DD0050/DD0050','DD0100/DD0100','DD0150/DD0150','DD0187/DD0187']
-axis = 'z'
+axis = 'x'
 fields_list = ['number_density', 'temperature', 'H_p0_number_density', 'O_Number_Density']
 nrow,ncol = 4,4
+fsize = 4
 
 #nrow,ncol = 2,2
 #fns = ['DD0100/DD0100','DD0150/DD0150']
 #fields_list = ['number_density','temperature']
 
-fig = plt.figure()
+fig = plt.figure(figsize=(fsize*nrow, fsize*ncol))
 
 # See http://matplotlib.org/mpl_toolkits/axes_grid/api/axes_grid_api.html
 # These choices of keyword arguments produce a four panel plot with a single
 # shared narrow colorbar on the right hand side of the multipanel plot. Axes
 # labels are drawn for all plots since we're slicing along different directions
 # for each plot.
-grid = AxesGrid(fig, (0.075,0.075,0.85,0.85),
+grid = AxesGrid(fig, (0.075,0.075,0.875,0.875),
                 nrows_ncols   = (nrow, ncol),
-                axes_pad      = 0.075,
+                axes_pad      = 0.1,
                 label_mode    = "L",
                 share_all     = True,
                 cbar_location = "right",
                 cbar_mode     = "edge",
-                cbar_size     = "7%",
-                cbar_pad      = "2%")
+                cbar_size     = "8%",
+                cbar_pad      = "0%")
 
 
-# region = ds.disk([0.5,0.5,0.5], [0,0,1], 2.0*yt.units.kpc, 2.0*yt.units.kpc)
 width  = (1.5,'kpc')
-fig.set_size_inches(8*nrow,8*ncol)
 
 for i, fn in enumerate(fns):
     for j, field in enumerate(fields_list):
@@ -45,7 +47,6 @@ for i, fn in enumerate(fns):
 
         # Load the data and create a single plot
         gal = Galaxy(fn.split('/')[0])
-
         region = gal.ds.disk([0.5,0.5,0.5], [0,0,1], 2.0*yt.units.kpc, 2.0*yt.units.kpc)
 
         if field == 'number_density':
@@ -53,6 +54,8 @@ for i, fn in enumerate(fns):
                                   width = width, data_source = region)
             p.set_zlim(field, 1.0E-3, 1.0E3)
             p.set_cmap(field, 'viridis')
+            p.set_colorbar_label(field, r'n (cm$^{-3}$)')
+
             if gal.ds.parameters['NumberOfParticles'] > 0:
                 p.annotate_particles((1.0,'kpc'), p_size = 0.5, ptype='main_sequence_stars')
 
@@ -60,16 +63,19 @@ for i, fn in enumerate(fns):
             p = yt.SlicePlot(gal.ds, axis, field, width = width, data_source = region)
             p.set_zlim(field, 1.0E2, 1.0E7)
             p.set_cmap(field, 'RdYlBu_r')
+            p.set_colorbar_label(field, r'T (K)')
         elif field == 'H_p0_number_density':
             p = yt.ProjectionPlot(gal.ds, axis, field, width = width, data_source = region,
                                   weight_field = None)
             p.set_zlim(field, 1.0E16, 1.0E22)
             p.set_cmap(field, 'Greys')
+            p.set_colorbar_label(field, r'N$_{\rm HI}$ (cm$^{-2}$)')
         elif field == 'O_Number_Density':
             p = yt.ProjectionPlot(gal.ds, axis, field, width = width, data_source = region,
                                   weight_field=None)
             p.set_zlim(field, 1.0E12, 1.0E17)
             p.set_cmap(field, 'magma')
+            p.set_colorbar_label(field, r'N$_{\rm O}$ (cm$^{-2}$)')
 
         p.set_axes_unit('kpc')
         p.set_buff_size(1600)
@@ -78,13 +84,9 @@ for i, fn in enumerate(fns):
         plot = p.plots[field]
         plot.figure = fig
         plot.axes = grid[index].axes
-#        plot.cax = grid.cbar_axes[index]
 
         if ((index+1)%(nrow)):
-            # grid.cbar_axes[index/4].colorbar(plot.image)
-        # try
             plot.cax = grid.cbar_axes[index/nrow]
-        print np.size(grid.cbar_axes)
 
         # Finally, this actually redraws the plot.
         p._setup_plots()
@@ -92,5 +94,5 @@ for i, fn in enumerate(fns):
 #for cax in grid.cbar_axes:
 #    cax.toggle_label(True)
 #    cax.axis[cax.orientation].set_label('label')
-
+fig.set_size_inches(fsize*nrow,fsize*ncol)
 plt.savefig('multiplot_4x4_' + axis+ '.png')
