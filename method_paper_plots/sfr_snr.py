f0fbc16f143ee3444e9e9a31b6fe7c60f93b7b0f
Andrew Emerick <aemerick11@gmail.com>
2018-01-09 08:55:53 -0500

    optionally plot sfr and snr on same axis with scaling by 100 instead of separate axis

--- method_paper_plots/sfr_snr.py
+++ method_paper_plots/sfr_snr.py
@@ -6,9 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
+separate_axis = False
+
 filepath = '/mnt/ceph/users/emerick/enzo_runs/pleiades/starIC/run11_30km/final_sndriving'
-
-
 
 work_dir      = '/mnt/ceph/users/emerick/enzo_runs/pleiades/starIC/run11_30km/final_sndriving/'
 #work_dir      = '/mnt/ceph/users/emerick/enzo_runs/pleiades/starIC/run11/corrected_sndriving/'
@@ -35,11 +35,17 @@ fig.set_size_inches(8,8)
 
 plot_histogram(ax, times, sfr, lw = line_width, color = 'black', label = 'SFR')
 
+if separate_axis:
+    ax2 = ax.twinx()
+    norm = 1.0
+else:
+    ax2 = ax
+    norm = 100.0
+plot_histogram(ax2, times, snr*norm, lw = line_width, color = 'C3', label = 'SNRx100', ls = '--')
 
-ax2 = ax.twinx()
-plot_histogram(ax2, times, snr, lw = line_width, color = 'black', label = 'SNR', ls = '--')
-ax2.semilogy()
-ax2.set_ylabel(r'SNR (yr$^{-1}$)')
+if separate_axis:
+    ax2.semilogy()
+    ax2.set_ylabel(r'SNR (yr$^{-1}$)')
 
 ax.set_xlabel(r'Time (Myr)')
 ax.set_ylabel(r'SFR (M$_{\odot}$ yr$^{-1}$)')
@@ -50,8 +56,16 @@ ax.set_xlim(np.min(times),np.max(times))
 plt.tight_layout()
 plt.minorticks_on()
 
-ax.set_ylim(0.5E-6,5.0E-4)
+if separate_axis:
+    ax.set_ylim(2E-7,5.0E-4)
+    ax2.set_ylim(ax.get_ylim())
+else:
+    ax.set_ylim(1.0E-5, 5.0E-4)
 
-ax2.set_ylim(ax.get_ylim())
+if separate_axis:
+    outname = 'sfr_snr_2ax.png'
+else:
+    ax.legend(loc = 'upper right')
+    outname = 'sfr_snrx100.png'
 
-fig.savefig('sfr_snr.png')
+fig.savefig(outname)
