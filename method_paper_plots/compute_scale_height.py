8a9d33873e64499a3d3e62d96fc4000cd3032514
Andrew Emerick <aemerick11@gmail.com>
2018-04-27 17:29:06 -0400

    first commit. Also updated this to compute scale heights for each phase and generalized plotting interface

--- method_paper_plots/compute_scale_height.py
+++ method_paper_plots/compute_scale_height.py
@@ -15,6 +15,8 @@ import itertools
 import os
 import glob
 
+import sys
+
 @derived_field(name = "mag_z", units = "pc")
 def _mag_z(field, data):
     return np.abs(data['cylindrical_z']).convert_to_units("pc")
@@ -43,6 +45,8 @@ def compute_scale_height(region, rbins):
 
     return scale_height
 
+_RBINS = np.arange(0,601,20)*yt.units.pc
+
 def _parallel_loop(dsname):
 
 #    for dsname in ds_list:
@@ -50,11 +54,15 @@ def _parallel_loop(dsname):
     d  = {dsname : {}}
     ds = yt.load(dsname + '/' + dsname)
 
-    rbins = np.arange(0, 601, 20)*yt.units.pc
+    rbins  = _RBINS
     region = ds.disk([0.5,0.5,0.5],[0,0,1], np.max(rbins), 2.0*yt.units.kpc)
 
     d[dsname]['scale_height'] = compute_scale_height(region, rbins.value)
     d[dsname]['times'] = ds.current_time.convert_to_units('Myr')
+    d[dsname]['phases'] = {}
+    for phase in ['Molecular','CNM','WNM','WIM','HIM']:
+        phase_region = region.cut_region(ISM[phase])
+        d[dsname]['phases'][phase] = compute_scale_height(phase_region, rbins.value)
     del(region)
     del(ds)
 
@@ -98,18 +106,28 @@ def compute_all_data(nproc = 28):
             print r.keys()[0], r[r.keys()[0]].keys(), it
 
             all_data[r.keys()[0]] = {}
-            all_data[r.keys()[0]]['scale_height'] = r[r.keys()[0]]['scale_height']
-            all_data['times'][it] = r[r.keys()[0]]['times']
+            for k in r[r.keys()[0]].keys():
+                all_data[r.keys()[0]][k] = r[r.keys()[0]][k]
+#            all_data[r.keys()[0]]['scale_height'] = r[r.keys()[0]]['scale_height']
+#            all_data[r.keys()[0]]['phases'] = r[r.keys()[0]]['phases']
+#            all_data['times'][it] = r[r.keys()[0]]['times']
             it = it +1
         del(results)
 
-    all_data['xbins'] = np.arange(0.0,601.0,20)*yt.units.pc
+    all_data['xbins'] = _RBINS
 
     dd.io.save('scale_height_data.h5', all_data)
 
     return all_data
 
-def plot_all_data():
+def plot_all_data(t_o = 46, dt = 20, t = [150,300,500]):
+    """
+    Plot data that must already be generated and saved as scale_height_data.h5.
+
+    t_o is the initial time (first star formation)
+    t is the list of additional times to plot using a time average of total
+    width dt, centered on each t. t is relative to t_o
+    """
 
     data = dd.io.load('scale_height_data.h5')
     data_list = np.array([x for x in data.keys() if 'DD' in x])
@@ -125,29 +143,35 @@ def plot_all_data():
     plot_histogram(ax, data['xbins'], data['DD0001']['scale_height'],
                       lw = line_width, color = 'black', label = 'Initial Conditions', ls = '--')
 
-    plot_histogram(ax, data['xbins'], data['DD0061']['scale_height'],
+    ds_o = data.keys()[np.argmin(np.abs(data['times'] - t_o))] # name of initial dataset
+
+    plot_histogram(ax, data['xbins'], data[ds_o]['scale_height'],
                       lw = line_width, color = plasma(0.0), label = '0 Myr', ls = '-')
 
-    x, avg, min, max, std = cta.compute_time_average(['scale_height'], dir = '.', data_list = data_list, sc_data=data,  #data_list = 'scale_height_data.h5',
-                                                     self_contained = True, x_field = None,
-                                                     times = data['times'], tmin = 176.0, tmax = 216.0)
+    i = 0
+    ls = ['--','-.',':']*3
+    for t_center in t:
+        tmin = t_center - 0.5*dt + t_o
+        tmax = t_center + 0.5*dt + t_o
+        x, avg, min, max, std = cta.compute_time_average(['scale_height'], dir = '.', data_list = data_list, sc_data=data,  #data_list = 'scale_height_data.h5',
+                                                         self_contained = True, x_field = None,
+                                                         times = data['times'], tmin = tmin, tmax = tmax)
 
-#    for name in ['DD0000','DD0050','DD0100','DD0150','DD0180']:
-    plot_histogram(ax, data['xbins'], avg,
-                               lw = line_width, color = plasma(0.25), ls = '--', label = '150 Myr')
+        plot_histogram(ax, data['xbins'], avg,
+                                   lw = line_width, color = plasma((i+1) / (1.0*len(t)+1)), ls = ls[i], label = '%3i Myr'%(t_center+t_o))
 
-    x, avg, min, max, std = cta.compute_time_average(['scale_height'], dir = '.', data_list = data_list, sc_data = data, #data_list = 'scale_height_data.h5',
-                                                     self_contained = True, x_field = None,
-                                                     times = data['times'], tmin = 326.0, tmax = 366.0)
+#    x, avg, min, max, std = cta.compute_time_average(['scale_height'], dir = '.', data_list = data_list, sc_data = data, #data_list = 'scale_height_data.h5',
+#                                                     self_contained = True, x_field = None,
+#                                                     times = data['times'], tmin = 326.0, tmax = 366.0)
 
-    plot_histogram(ax, data['xbins'], avg,
-                               lw = line_width, color = plasma(0.5), label = '300 Myr', ls = '-.')
+#    plot_histogram(ax, data['xbins'], avg,
+#                               lw = line_width, color = plasma(0.5), label = '300 Myr', ls = '-.')
 
-    x, avg, min, max, std = cta.compute_time_average(['scale_height'], dir = '.', data_list = data_list, sc_data = data, # data_list = 'scale_height_data.h5',
-                                                     self_contained = True, x_field = None,
-                                                     times = data['times'], tmin = 526.0, tmax = 566.0)
-    plot_histogram(ax, data['xbins'], avg,
-                               lw = line_width, color = plasma(0.75), label = '500 Myr', ls = ':')
+#    x, avg, min, max, std = cta.compute_time_average(['scale_height'], dir = '.', data_list = data_list, sc_data = data, # data_list = 'scale_height_data.h5',
+#                                                     self_contained = True, x_field = None,
+#                                                     times = data['times'], tmin = 526.0, tmax = 566.0)
+#    plot_histogram(ax, data['xbins'], avg,
+#                               lw = line_width, color = plasma(0.75), label = '500 Myr', ls = ':')
 
 
     ax.set_xlabel(r'R (pc)')
@@ -163,6 +187,14 @@ def plot_all_data():
     return
 
 if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
 #    compute_all_data()
 
-    plot_all_data()
+        plot_all_data()
+    else:
+        if len(sys.argv < 4):
+            print "if passing arguments need to pass at least 3, t_o, dt, and then at least 1 time to plot (prefereably more)"
+            raise ValueError
+        # assume all args are provided:
+        plot_all_data( t_o = sys.argv[1], dt = sys.argv[2], t = sys.argv[3:])
