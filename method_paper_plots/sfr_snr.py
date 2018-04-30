bd33d768dae6fe6634e3f5cf24a1556e96431f2f
Andrew Emerick <aemerick11@gmail.com>
2018-04-30 18:35:03 -0400

    generalized plotting interface

--- method_paper_plots/sfr_snr.py
+++ method_paper_plots/sfr_snr.py
@@ -8,78 +8,72 @@ import glob
 import sys
 
 
-separate_axis = False
+def plot(work_dir, separate_axis = False, TMAX = 500.0, outdir = './'):
 
-TMAX = 500.0
+    data_list = np.sort(glob.glob(work_dir + 'DD*_galaxy*.h5'))
+    data = dd.io.load( data_list[-1] )
 
-if len(sys.argv) > 1:
-    work_dir = sys.argv[1]
-else:
-    work_dir      = '/mnt/ceph/users/emerick/enzo_runs/pleiades/starIC/run11_30km/final_sndriving/'
-#work_dir      = '/mnt/ceph/users/emerick/enzo_runs/pleiades/starIC/run11/corrected_sndriving/'
+    all_data = {}
+    times = data['time_data']['time'] / 1.0E6
+    sfr   = (data['time_data']['SFR'])
+    snr   = (data['time_data']['SNII_snr'])
+    agbr  = data['time_data']['AGB_rate']
 
-data_list = np.sort(glob.glob(work_dir + 'DD*_galaxy*.h5'))
-data = dd.io.load( data_list[-1] )
-#data_list, times = utilities.select_data_by_time(dir = work_dir,
-#                                                 tmin=0.0,tmax=1000)
+    times = times - times[0]
 
-all_data = {}
-times = data['time_data']['time'] / 1.0E6
-sfr   = (data['time_data']['SFR'])
-snr   = (data['time_data']['SNII_snr'])
-agbr  = data['time_data']['AGB_rate']
-#sfr = np.ones(np.size(data_list))
-#snr = np.ones(np.size(data_list))
-#for i,k in enumerate(data_list):
-#    sfr[i] = dd.io.load(k, '/meta_data/SFR')
-#    snr[i] = dd.io.load(k, '/time_data/SNII_snr')[-1]
+    fig, ax = plt.subplots()
+    fig.set_size_inches(8,8)
 
-times = times - times[0]
+    plot_histogram(ax, times, sfr, lw = line_width, color = 'black', label = 'SFR')
 
-fig, ax = plt.subplots()
-fig.set_size_inches(8,8)
+    if separate_axis:
+        ax2 = ax.twinx()
+        norm = 1.0
+    else:
+        ax2 = ax
+        norm = 100.0
+    plot_histogram(ax2, times, snr*norm, lw = line_width, color = 'C3', label = 'SNRx100', ls = '--')
 
-plot_histogram(ax, times, sfr, lw = line_width, color = 'black', label = 'SFR')
+    if separate_axis:
+        ax2.semilogy()
+        ax2.set_ylabel(r'SNR (yr$^{-1}$)')
 
-if separate_axis:
-    ax2 = ax.twinx()
-    norm = 1.0
-else:
-    ax2 = ax
-    norm = 100.0
-plot_histogram(ax2, times, snr*norm, lw = line_width, color = 'C3', label = 'SNRx100', ls = '--')
+    ax.set_xlabel(r'Time (Myr)')
+    ax.set_ylabel(r'SFR (M$_{\odot}$ yr$^{-1}$)')
+    ax.semilogy()
 
-if separate_axis:
-    ax2.semilogy()
-    ax2.set_ylabel(r'SNR (yr$^{-1}$)')
+    ax.set_xlim(np.min(times),np.min([TMAX,np.max(times)]))
+    #ax.legend(loc='best')
+    plt.tight_layout()
+    plt.minorticks_on()
 
-ax.set_xlabel(r'Time (Myr)')
-ax.set_ylabel(r'SFR (M$_{\odot}$ yr$^{-1}$)')
-ax.semilogy()
+    if separate_axis:
+        ax.set_ylim(2E-7,5.0E-4)
+        ax2.set_ylim(ax.get_ylim())
+    else:
+        ax.set_ylim(1.0E-5, 5.0E-4)
 
-ax.set_xlim(np.min(times),np.min([TMAX,np.max(times)]))
-#ax.legend(loc='best')
-plt.tight_layout()
-plt.minorticks_on()
+    if separate_axis:
+        outname = 'sfr_snr_2ax.png'
+    else:
+        ax.legend(loc = 'upper right')
+        outname = 'sfr_snrx100.png'
 
-if separate_axis:
-    ax.set_ylim(2E-7,5.0E-4)
-    ax2.set_ylim(ax.get_ylim())
-else:
-    ax.set_ylim(1.0E-5, 5.0E-4)
+    fig.savefig(outdir + outname)
 
-if separate_axis:
-    outname = 'sfr_snr_2ax.png'
-else:
+
+    plot_histogram(ax2, times, agbr*10, lw = line_width, color = 'C2', label = 'AGB Rate x 10', ls = '-')
     ax.legend(loc = 'upper right')
-    outname = 'sfr_snrx100.png'
 
-fig.savefig(outname)
+    fig.savefig(outdir + 'sfr_snrx100_agb.png')
 
+    print np.min(agbr), np.max(agbr), np.average(agbr)
+    return
 
-plot_histogram(ax2, times, agbr*10, lw = line_width, color = 'C2', label = 'AGB Rate x 10', ls = '-')
-ax.legend(loc = 'upper right')
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        work_dir = sys.argv[1]
+    else:
+        work_dir      = '/mnt/ceph/users/emerick/enzo_runs/pleiades/starIC/run11_30km/final_sndriving/'
 
-fig.savefig('sfr_snrx100_agb.png')
-
-print np.min(agbr), np.max(agbr), np.average(agbr)
+    plot(work_dir)
