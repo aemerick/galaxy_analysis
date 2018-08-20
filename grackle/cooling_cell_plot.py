c47164e584ac2979ad465c11e5c08a66addf23d4
Andrew Emerick <aemerick11@gmail.com>
2018-08-20 02:19:04 -0400

    including simulation points in graph and improving styles. Adding contours

--- grackle/cooling_cell_plot.py
+++ grackle/cooling_cell_plot.py
@@ -17,7 +17,7 @@ def bins_from_centers(x):
 
 def plot_2d_histogram(datafile = 'all_runs_d_12.20.dat'):
 
-    ylabel = 'log(H- Photo-Diss. Scale Factor)'
+    ylabel = r'log(H$^{-}$ Photodetachment Scale Factor)'
     xlabel = "log(LW Scale Factor)"
 
 
@@ -26,14 +26,15 @@ def plot_2d_histogram(datafile = 'all_runs_d_12.20.dat'):
 
     k27      = data[:,0]
     LW       = data[:,1]
-    k27_vals = np.linspace(np.log10(np.min(k27)), np.log10(np.max(k27)),
+    k27_centers = np.linspace(np.log10(np.min(k27)), np.log10(np.max(k27)),
                            int(np.sqrt(np.size(k27) )))
-    k27_vals = bins_from_centers(k27_vals)
-    LW_vals  = np.linspace(np.log10(np.min(LW)), np.log10(np.max(LW)),
+    k27_vals = bins_from_centers(k27_centers)
+    LW_centers  = np.linspace(np.log10(np.min(LW)), np.log10(np.max(LW)),
                            int(np.sqrt(np.size(LW))))
-    LW_vals = bins_from_centers(LW_vals)
+    LW_vals = bins_from_centers(LW_centers)
 
     k27_mesh, LW_mesh = np.meshgrid(LW_vals, k27_vals)
+    k27_center_mesh, LW_center_mesh = np.meshgrid(LW_centers, k27_centers)
 
     #f_H2[data['k27'] == 1.58489319] = 100.0 # flag to figure out orientation
     f_H2 = data[:,2]
@@ -46,8 +47,8 @@ def plot_2d_histogram(datafile = 'all_runs_d_12.20.dat'):
     img1 = ax.pcolormesh(10.0**(LW_mesh),
                          10.0**(k27_mesh),
                          np.log10(z_mesh.T), cmap = 'magma',
-                         vmin = np.min(np.log10(z_mesh)),
-                         vmax = np.max(np.log10(z_mesh)))
+                         vmin = -9,
+                         vmax = -2.8)
     ax.semilogx()
     ax.semilogy()
     ax.set_xlabel(xlabel)
@@ -56,6 +57,11 @@ def plot_2d_histogram(datafile = 'all_runs_d_12.20.dat'):
     divider = make_axes_locatable(ax)
     cax1 = divider.append_axes('right', size = '5%', pad = 0.05)
     fig.colorbar(img1, cax=cax1, label = r'log(f$_{\rm H_2}$)')
+
+    ax.contour( 10.**(LW_center_mesh), 10.0**(k27_center_mesh), np.log10(z_mesh.T),
+                levels = [-8,-7,-6,-5,-4,-3], colors = 'black',
+                linewidths = 3, linestyles = '-.')
+    ax.scatter( [1,1,100,100], [1,100,1,100], s = 250, marker = "*", color = "white")
 
     plt.minorticks_on()
 
