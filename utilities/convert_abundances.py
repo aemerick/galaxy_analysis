3384a027fc152c3c19ce776685251367c5057ed0
Andrew Emerick <aemerick11@gmail.com>
2018-11-14 22:55:58 -0500

    helper functions for obtaining yields and yield ratios from yield tables

--- utilities/convert_abundances.py
+++ utilities/convert_abundances.py
@@ -7,6 +7,54 @@ from galaxy_analysis.static_data import \
      AMU, \
      SOLAR_ABUNDANCE
 
+from onezone import data_tables as DT
+
+SN_YIELD_TABLE           = DT.StellarYieldsTable('SNII')
+WIND_YIELD_TABLE         = DT.StellarYieldsTable('wind')
+MASSIVE_STAR_YIELD_TABLE = DT.StellarYieldsTable('massive_star')
+
+def return_yields(yield_type, M, Z, species):
+    """
+    Get the mass yields for a list of species for a star of mass M
+    and metallicity Z as given in the adopted yield tables. This can
+    be done for three yield_types: 
+        'SNII' : Just yield from supernova associated with star (if any)
+        'wind' : Yield from winds from star (AGB or otherwise)
+        'all'  : sum of 'SNII' and 'wind'
+
+    Stars above M > 25.0 are assumed to have NO CCSN yields
+    """
+
+
+    interp = {'SNII' : lambda x,y : SN_YIELD_TABLE.interpolate([x,y], species),
+              'wind' : lambda x,y : WIND_YIELD_TABLE.interpolate([x,y], species),
+              'massive_star' : lambda x,y : MASSIVE_STAR_YIELD_TABLE.interpolate([x,y], species)}
+
+    interp['all'] = lambda x,y : interp['SNII'](x,y) + interp['wind'](x,y)
+
+    if M > 25.0:
+        yield_type = 'massive_star'
+
+    return np.array(interp[yield_type](M,Z))
+
+def get_yield_ratio(ele1, ele2, M, Z, yield_type = 'all'):
+    """
+    Get the abundance ratio between two elements for a star of mass M
+    and metallicity Z as given in the adopted yield tables. This can
+    be done for three yield_types:
+        'SNII' : Just yield from supernova associated with star (if any)
+        'wind' : Yield from winds from star (AGB or otherwise)
+        'all'  : sum of 'SNII' and 'wind'
+
+    Stars above M > 25.0 are assumed to have NO CCSN yields
+    """
+
+    yields = return_yields(yield_type, M, Z, [ele1, ele2])
+
+    yields = (yields * yt.units.Msun).to('g').value
+
+    return abundance_ratio((ele1,yields[0]), (ele2,yields[1]), input_type = 'mass')
+
 def elemental_abundance(element, mass):
     n = mass / (MOLECULAR_WEIGHT[element] * AMU)
 
