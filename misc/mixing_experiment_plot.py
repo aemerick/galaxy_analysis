8c1119ff7d9327682b0de7aa214a59301a0cba38
Andrew Emerick <aemerick11@gmail.com>
2019-09-25 23:38:36 -0400

    updates

--- misc/mixing_experiment_plot.py
+++ misc/mixing_experiment_plot.py
@@ -14,7 +14,9 @@ import os
 import glob
 import sys
 
-def plot_field(dsname, mass_field):
+def plot_field(dsname, mass_field, paperstyle=False, show_colorbar=True):
+
+    fontsize = 'large'
 
     field_to_plot   = mass_field + "_disk_fraction"
     field_parameter = "total_field_mass"
@@ -55,6 +57,7 @@ def plot_field(dsname, mass_field):
     axis = 2
     #print np.shape(cg[field_to_plot])
     proj_data       = np.sum(cg[field_to_plot], axis=axis)
+    n_proj_data     = np.max(cg['number_density'].value, axis=axis)
     #print np.shape(proj_data)
 
     #print np.min(proj_data), np.max(proj_data)
@@ -66,9 +69,11 @@ def plot_field(dsname, mass_field):
         extent = [-dist[1],dist[1],-dist[2],dist[2]]
         proj_data = proj_data.T
         fsize  = (12.5,1)
+        n_proj_data = n_proj_data.T
 
     pp = plt.imshow( proj_data, norm = LogNorm(), extent=extent)
     pp.set_clim(1.0E-10,1.0E-1)
+    pp.set_cmap("magma")
     #plt.tight_layout()
     #pp.axes.set_xticks([-600,-400,-200,0,200,400,600])
     pp.axes.yaxis.set_visible(False)
@@ -76,8 +81,6 @@ def plot_field(dsname, mass_field):
     pp.axes.set_frame_on(False)
     pp.figure.set_size_inches(fsize)
     pp.figure.patch.set_facecolor('black')
-
-
     # annotate the size
     anncoord = (0.9,0.1)
     dim = np.shape(proj_data)
@@ -90,7 +93,58 @@ def plot_field(dsname, mass_field):
                      xy=anncoord, xycoords='axes fraction',
                      xytext=anncoord, textcoords='axes fraction', color = "white",
                      # arrowprops=dict(facecolor='black', shrink=0.05),
-                     horizontalalignment='right', verticalalignment='top')
+                     horizontalalignment='right', verticalalignment='top', fontsize = fontsize)
+
+    start = 220.0
+    anncoord2 = (0.225,0.96)
+    if not paperstyle:
+        pp.axes.annotate("%.1f Myr"%(gal.ds.current_time.to('Myr').value - start),
+                        xy=anncoord2, xycoords='axes fraction',
+                        xytext=anncoord2, textcoords='axes fraction', color = "white",
+                    # arrowprops=dict(facecolor='black', shrink=0.05),
+                        horizontalalignment='right', verticalalignment='top', fontsize = fontsize)
+
+
+#    pp.figure.savefig("./mixing_plots/" + dsname + "_" + mass_field + "_disk_fraction.png")
+    outname = "./mixing_plots/" + dsname + "_" + mass_field + "_disk_fraction.png"
+    if show_colorbar:
+        cax = plt.colorbar(orientation="horizontal")
+        cticks =  [1.0E-8, 1.0E-6, 1.0E-4, 1.0E-2, 1.0]
+        cticks =  [1.0E-9, 1.0E-7, 1.0E-5, 1.0E-3, 1.0E-1]
+        cax.set_ticks(cticks)
+        cax.set_ticklabels( [r"10$^{%2i}$"%(np.log10(x)) for x in cticks] )
+        cax.set_label("Tracer Fraction (Arbitrary Units)")
+
+
+    plt.savefig(outname, bbox_inches="tight", pad_inches = 0.0)
+    plt.close()
+
+############
+    pp = plt.imshow( n_proj_data, norm = LogNorm(),
+                     extent=extent)
+    pp.set_clim(1.0E-4,1.0E3)
+    #plt.tight_layout()
+    #pp.axes.set_xticks([-600,-400,-200,0,200,400,600])
+    pp.axes.yaxis.set_visible(False)
+    pp.axes.xaxis.set_visible(False)
+    pp.axes.set_frame_on(False)
+    pp.figure.set_size_inches(fsize)
+    pp.figure.patch.set_facecolor('black')
+    # annotate the size
+    anncoord = (0.9,0.1)
+    dim = np.shape(n_proj_data)
+    x  = 0.5
+    di = 250.0 / ( 1.8 )
+    pp.axes.plot( [dim[0]*x, dim[0]*x + di],
+                  [-dim[1]*0.825]*2, lw = 3, color = "white")
+
+
+    pp.axes.annotate('250 pc',
+                     xy=anncoord, xycoords='axes fraction',
+                     xytext=anncoord, textcoords='axes fraction', color = "white",
+                     # arrowprops=dict(facecolor='black', shrink=0.05),
+                     horizontalalignment='right', verticalalignment='top', fontsize = fontsize)
+
 
     start = 220.0
     anncoord2 = (0.225,0.96)
@@ -98,18 +152,26 @@ def plot_field(dsname, mass_field):
                     xy=anncoord2, xycoords='axes fraction',
                     xytext=anncoord2, textcoords='axes fraction', color = "white",
                 # arrowprops=dict(facecolor='black', shrink=0.05),
-                    horizontalalignment='right', verticalalignment='top')
+                    horizontalalignment='right', verticalalignment='top', fontsize = fontsize)
 
 
 #    pp.figure.savefig("./mixing_plots/" + dsname + "_" + mass_field + "_disk_fraction.png")
-    outname = "./mixing_plots/" + dsname + "_" + mass_field + "_disk_fraction.png"
+    outname = "./mixing_plots/" + dsname + "_" + "ndens.png"
+    if show_colorbar:
+        cax = plt.colorbar(orientation = "horizontal")
+        cticks =  [1.0E-4, 1.0E-2, 1.0, 100.0]
+        cax.set_ticks(cticks)
+        cax.set_ticklabels( [r"10$^{%2i}$"%(np.log10(x)) for x in cticks] )
+        cax.set_label(r"n (cm$^{-3}$)")
+
     plt.savefig(outname, bbox_inches="tight", pad_inches = 0.0)
+
     return
 
 
 if __name__ == "__main__":
 
-    mass_field = "Na_Mass"
+    mass_field = "C_Mass"
     nproc      = 1
     ds_list    = None
 
