a725acb2d9dfc30339541cb282ae63f1cced5f55
Andrew Emerick <aemerick11@gmail.com>
2019-11-22 11:55:49 -0800

    first commit. simple script to handle python2 to 3 issues in strings / dict keys in the way I had been sotring data before.

---
+++ misc/convert.py
@@ -0,0 +1,29 @@
+def convert_keys_to_string(dictionary):
+    """Recursively converts dictionary keys to strings."""
+    if not isinstance(dictionary, dict):
+        return dictionary
+    return dict((str(k), convert_keys_to_string(v)) 
+        for k, v in dictionary.items())
+
+import glob, os
+import numpy as np
+import deepdish as dd
+import copy
+
+files = np.sort(glob.glob('DD????_galaxy_data.h5'))
+
+if not (os.path.exists('py3temp/')):
+    os.mkdir('py3temp/')
+
+for f in files:
+    if os.path.exists('py3temp/'+f):
+        continue
+    data = dd.io.load(f)
+    data2 = copy.deepcopy(data)
+    data3 = convert_keys_to_string(data2)
+    dd.io.save('py3temp/' + f,data3)
+    del(data)
+    del(data2)
+    del(data3)
+    print(f)
+
