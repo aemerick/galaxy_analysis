e3fe8e12ab610329134848832105470cb2b4dbc0
Andrew Emerick <aemerick11@gmail.com>
2019-12-19 17:29:17 -0800

    error checking

--- misc/convert.py
+++ misc/convert.py
@@ -1,3 +1,10 @@
+"""
+Fixes issues in how I stored strings / complex keys in
+dictionaries in HDF5 files in python2 that can be weird
+in python3.
+"""
+
+
 def convert_keys_to_string(dictionary):
     """Recursively converts dictionary keys to strings."""
     if not isinstance(dictionary, dict):
@@ -18,7 +25,10 @@ if not (os.path.exists('py3temp/')):
 for f in files:
     if os.path.exists('py3temp/'+f):
         continue
-    data = dd.io.load(f)
+    try:
+        data = dd.io.load(f)
+    except:
+        print("--------------Loading failed for file", f)
     data2 = copy.deepcopy(data)
     data3 = convert_keys_to_string(data2)
     dd.io.save('py3temp/' + f,data3)
