735fba89b7075e8ea7e004f53cd3f25aeb639abb
Andrew Emerick <aemerick11@gmail.com>
2020-06-29 18:55:42 -0500

    updates

--- gizmo/rate_comparison.py
+++ gizmo/rate_comparison.py
@@ -8,12 +8,13 @@ import utilities as ga_ut
 
 
 FIRE_elements      = ['h','he','c','n','o','ne','mg','si','s','ca','fe']
+FIRE_metals = ['c','n','o','ne','mg','si','s','ca','fe']
 
 #
 # wrapper to load data set and set FIRE
 # abundance tables
 #
-def load_with_FIRE(sim_index, wdir, return_model = False):
+def load_with_FIRE(sim_index, wdir, return_model = False, model_Z = 1.0):
     """
     Convenience wrapper to load a dataset and set the appropriate initial
     abundances and FIRE yield model tables to do age tracer postprocessing
@@ -29,15 +30,15 @@ def load_with_FIRE(sim_index, wdir, return_model = False):
     initial_abundances['metals'] = np.average(initial_part['gas'].prop('massfraction.metals'))
 
 
-    part = ga.io.Read.read_snapshots(['gas','stars'],
+    part = ga.io.Read.read_snapshots(['gas','star'],
                               'index',
                               sim_index,
                               simulation_directory = wdir)
 
 
-    FIRE_yield_model = gizmo.agetracers.FIRE2_yields(model_Z = 1.0 # yield table metallicity in solar units,
+    FIRE_yield_model = ga.agetracers.FIRE2_yields(model_Z = model_Z # yield table metallicity in solar units,
                                             )
-    FIRE_yield_table = gizmo.agetracers.construct_yield_table(FIRE_yield_model, # object with a `yields` function
+    FIRE_yield_table = ga.agetracers.construct_yield_table(FIRE_yield_model, # object with a `yields` function
                                                               part.ageprop.age_bins/1000.0) # in Gyr
 
     part.set_yield_table(FIRE_yield_table,          # the 2D table
@@ -79,7 +80,7 @@ def bin_error(part, element, particle_type='star',
 
     stats = {'bins' : bins,
              'hist' : hist,
-             'cbins': 0.5*(bins[1:]-bins[:-1]),
+             'cbins': 0.5*(bins[1:]+bins[:-1]),
              'median' : np.percentile(error,50.0),
              'onesigma' : np.percentile(error,68.0),
              'twosigma' : np.percentile(error,95.0),
@@ -96,9 +97,9 @@ def compute_all_errors(part,particle_type='star'):
     all_hist  = {}
     all_stats = {}
 
-    for e in FIRE_elements:
-
+    for e in FIRE_metals:
         all_stats[e] = bin_error(part,e,particle_type)
+
     return all_stats
 
 
@@ -113,19 +114,29 @@ if __name__ == "__main__":
     for runname in runs.keys():
         all_part[runname] = load_with_FIRE(600,"./" + runname)
         all_data[runname] = {}
-        all_data[runname] = compute_all_errors(part,'star')
+        all_data[runname] = compute_all_errors(all_part[runname],'star')
 
+    fs = 5
     fig,ax = plt.subplots(3,3, sharex=True,sharey=True)
-    fig.set_size_inches(18,18)
+    fig.set_size_inches(3*fs,3*fs)
     fig.subplots_adjust(hspace=0,wspace=0)
 
 
     axi, axj = 0,0
-    for e in elements:
-        for runname in runs.keys():
+    for e in FIRE_metals:
+        axindex = (axi,axj)
 
-            ax.plot(all_data[runname]['cbins'], np.cumsum(all_data[runname]['hist']) / (1.0*np.sum(all_data[runname]['hist'])),
+        for runname in runs.keys():
+        
+            ploty = np.cumsum(all_data[runname][e]['hist']*1.0)
+            ploty = ploty/ploty[-1]
+
+            ax[axindex].plot(all_data[runname][e]['cbins'], ploty,
                     lw = 3, label = runs[runname])
+        ax[axindex].set_ylim(0.0,1.0)
+        ax[axindex].set_xlim(0.001, 1.0)
+        ax[axindex].semilogx()
+ 
 
         axj = axj + 1
         if axj >= 3:
