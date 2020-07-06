37a3ef0cc58c299b56bff40d4b25659df0e627e8
Andrew Emerick <aemerick11@gmail.com>
2020-07-06 16:57:04 -0500

    updates to allow for saving of H5 files. still working out kinks

--- cosmology/plot_testing.py
+++ cosmology/plot_testing.py
@@ -4,27 +4,67 @@ matplotlib.use('agg')
 import yt
 import numpy as np
 import glob
-import sys
+import sys, os
 from galaxy_analysis.yt_fields import field_generators as fg
 
+import h5py
+import deepdish as dd
 
+
+def get_output_H5(outdata_path):
+    if os.path.isfile(outdata_path):
+        outH5 = dd.io.load(outdata_path)
+    else:
+        outH5 = {}
+
+    return outH5
+
+def save_to_file(outdata_path, sp, data, fields):
+
+    outH5 = get_output_H5(outdata_path)
+
+    for f in sp.frb.data.keys():
+        if not f[1] in outH5.keys():
+            outH5[f[1]] = {}
+        outH5[f[1]][weight_field] = sp.frb.data[f]
+
+    if not 'particle_positions' in outH5.keys():
+        outH5['paritcle_positions'] = {}
+
+    for ax in ['x','y','z']:
+        outH5['particle_positions'][ax] = data[('all_tars','particle_position_'+ax)].to('kpc')/sp.width.to('kpc')
+
+    dd.io.save(outdata_path, outH5)
+
+    return outH5
+
+
+
+save_frb = True
 
 yt.enable_parallelism()
 
-#if len(sys.argv) > 1:
-#    imin = int(sys.argv[1])
-#    imax = int(sys.argv[2])
-#else:
+weight_field = "number_density"
 
-imin = -2
-imax = -1
+imin,imax = None, None
+if len(sys.argv) > 1:
+    imin = int(sys.argv[1])
+    imax = int(sys.argv[2])
+if len(sys.argv) > 3:
+    if str(sys.argv[3]) != "&":
+        weight_field = str(sys.argv[3])
 
+if imin is None:
+    dsfiles = [np.sort(glob.glob("DD????/DD????"))[-1]]
+else:
+    dsfiles = ["DD%0004i/DD%0004i"%(i,i) for i in np.arange(imin,imax+1,1)]
 
-dsfiles = np.sort(glob.glob("DD????/DD????"))
+if yt.is_root():
+    print(dsfiles)
 
 
 fields = [('gas','number_density'),'Temperature',
-#          ('gas','photo_gamma'),
+          ('gas','photo_gamma'),
           ('gas','metallicity'),
           ('gas','C_over_N'),
           ('gas','C_over_H'),
@@ -92,10 +132,8 @@ def format_plot(ds, sp, fields):
 
     return
 
+for dsname in dsfiles:
 
-weight_field = 'metallicity'
-
-for dsname in dsfiles[imin:imax]:
     ds = yt.load(dsname)
     fg.generate_derived_fields(ds)
     ds = yt.load(dsname)
@@ -103,17 +141,20 @@ for dsname in dsfiles[imin:imax]:
 
     data = ds.all_data()
 
-    px = data['particle_position_x'].to('kpc').value
-    py = data['particle_position_y'].to('kpc').value
-    pz = data['particle_position_z'].to('kpc').value
-    pid = data['particle_index']
+#    px = data['particle_position_x'].to('kpc').value
+#    py = data['particle_position_y'].to('kpc').value
+#    pz = data['particle_position_z'].to('kpc').value
+#    pid = data['particle_index']
 
-    select = (pid == 16777372)
+#    select = (pid == 16777372)
+#
+#    if np.size(pid[select]) == 0:
+#        select = (pid == 16777218)
 
-    if np.size(pid[select]) == 0:
-        select = (pid == 16777218)
+#    pcenter = np.array([px[select][0], py[select][0], pz[select][0]]) * yt.units.kpc
 
-    pcenter = np.array([px[select][0], py[select][0], pz[select][0]]) * yt.units.kpc
+    pcenter = np.array([44.04561761 ,13.03435496, 10.58975735]) * yt.units.kpc
+
 
     width= (data.right_edge[0].to('kpc') - data.left_edge[0].to('kpc'))
     sp = yt.ProjectionPlot(ds, 'z', fields, center=data.center, width=width,
@@ -121,12 +162,17 @@ for dsname in dsfiles[imin:imax]:
     format_plot(ds,sp,fields)
     if yt.is_root():
         sp.save("./proj/")
+
+    if yt.is_root() and save_frb:
+        outdata_path = './proj/' + str(ds) + '_Proj_data.h5'
+        save_to_file(outdata_path, sp, data, fields)
+
     del(sp)
 
     #
     # now make new regions
     #
-    for factor in [4,8,16,32]:
+    for factor in [2,4,8,16]:
         region = ds.region( pcenter, pcenter - width/(factor*2.0), pcenter + width / (factor*2.0))
         sp = yt.ProjectionPlot(ds, 'z', fields, center = region.center, width=width/(1.0*factor),
                                weight_field=weight_field, data_source=region)
@@ -135,6 +181,11 @@ for dsname in dsfiles[imin:imax]:
 
         if yt.is_root():
             sp.save("./proj/zoomx%i/"%(factor))
+
+        if yt.is_root() and save_frb:
+            outdata_path = "./proj/zoom%i/"%(factor) + str(ds) + '_Proj_data_zoom%i.h5'%(factor)
+            save_to_file(outdata_path, sp, region, fields)
+
         del(sp)
 
     del(ds)
