9aa54b21cc88182446de052478dab0f0b2f192eb
Andrew Emerick <aemerick11@gmail.com>
2020-07-22 09:59:28 -0500

    some more tweaking to try and get virial calc to work and improve useability

--- cosmology/find_halos.py
+++ cosmology/find_halos.py
@@ -3,11 +3,45 @@ import yt
 from yt.extensions.astro_analysis.halo_analysis.api import HaloCatalog
 from yt.data_objects.particle_filters import add_particle_filter
 from yt.extensions.astro_analysis.halo_finding.rockstar.api import RockstarHaloFinder
+from yt.data_objects.particle_filters import particle_filter
 from yt.fields.api import ValidateParameter
 
-import sys
+from yt.extensions.astro_analysis.halo_analysis.halo_recipes import add_recipe
+
+import sys, glob
 
 yt.enable_parallelism() # must be true for rockstar
+
+
+def my_calculate_virial_quantities(hc, fields,
+                                weight_field=None, accumulation=True,
+                                radius_field="radius", factor=2.0,
+                                overdensity_field=("gas", "overdensity"),
+                                critical_overdensity=200):
+    storage = "virial_quantities_profiles"
+    pfields = [field for field in fields if field != radius_field]
+
+    hc.add_callback("sphere", factor=factor)
+    if pfields:
+        hc.add_callback("profile", [radius_field], pfields,
+                        weight_field=weight_field,
+                        accumulation=accumulation,
+                        storage=storage)
+    hc.add_callback("profile", [radius_field], [overdensity_field],
+                    weight_field="cell_volume", accumulation=True,
+                    storage=storage)
+    hc.add_callback("virial_quantities", fields,
+                    overdensity_field=overdensity_field,
+                    critical_overdensity=critical_overdensity,
+                    profile_storage=storage)
+    hc.add_callback("delete_attribute", storage)
+    hc.add_callback("delete_attribute", "data_object")
+    return
+
+add_recipe("my_calculate_virial_quantities", my_calculate_virial_quantities)
+
+
+
 
 @particle_filter("dark_matter", requires=["particle_type"])
 def DarkMatter(pfilter,data):
@@ -47,8 +81,6 @@ def find_halos(dsname, wdir = './', *args, **kwargs):
 
     ds.parameters['min_dm_mass'] = min_dm_mass
 
-    ds.add_particle_filter('dark_matter')
-
     hc = HaloCatalog(data_ds = ds, finder_method = 'rockstar',
                      finder_kwargs = {'dm_only':True,
                                       'outbase' : ROCKSTAR_OUTPUT_PREFIX + str(ds),
@@ -78,16 +110,19 @@ def compute_virial_quantities(dsname, wdir = './', *args, **kwargs):
     data_ds = yt.load(wdir + dsname + '/' + dsname)
     halos_ds = yt.load(wdir + ROCKSTAR_OUTPUT_PREFIX + dsname + '/halos_0.0.bin')
 
-    hc = HaloCatalog(data_ds = data_ds, halos_ds = halos_ds)
+    hc = HaloCatalog(data_ds = data_ds, halos_ds = halos_ds,
+                     output_dir = wdir + HALOCATALOG_PREFIX + str(data_ds))
     hc.add_filter('quantity_value', 'particle_mass', '>', 1E4, 'Msun')
 
     if ('enzo','Density') in data_ds.field_list:
-        mass_field = 'matter_mass'
+        mass_field    = 'matter_mass'
+        radius_field  = "radius"
     else:
         # DM only simulation
-        mass_field = 'particle_mass'
+        mass_field = ('all',"particle_mass")
+        radius_field = ('all','particle_radius')
         
-    hc.add_recipe("calculate_virial_quantities", ["radius", mass_field ])
+    hc.add_recipe("my_calculate_virial_quantities", [radius_field, mass_field ], radius_field=radius_field)
     hc.create()
 
     return
@@ -96,6 +131,14 @@ def compute_virial_quantities(dsname, wdir = './', *args, **kwargs):
 if __name__ == '__main__':
 
     #dsname = 'DD0011'
-    for dsname in [str(sys.argv[1])]:
+
+    if len(sys.argv) > 2:
+        dsnames = ["RD%0004i"%(i) for i in np.arange(int(sys.argv[1]),int(sys.argv[2])+1,1)]
+    elif str(sys.argv[1]) == 'all':
+        dsnames = np.sort(glob.glob('RD????'))
+    else:
+        dsnames = [str(x) for x in sys.argv[1:]]
+
+    for dsname in dsnames:
         find_halos(dsname)
-        compute_virial_quantities(dsname)
+#        compute_virial_quantities(dsname)
